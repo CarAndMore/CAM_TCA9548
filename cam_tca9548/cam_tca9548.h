c4d6855b39e5c6615f5ca8a63d44ddeeb832a924
CarAndMore <44379612+CarAndMore@users.noreply.github.com>
2022-05-01 14:38:34 +0200

    Add files via upload

---
+++ cam_tca9548/cam_tca9548.h
@@ -0,0 +1,22 @@
+
+#ifndef CAM_TCA9548_h
+#define CAM_TCA9548_h
+#include "Arduino.h"
+
+class CAM_TCA9548 {
+  public:
+    bool begin(uint8_t ADDRESS = 0x70);
+
+    void setChannel(uint8_t Channel);
+    uint8_t getChanal(void);
+    bool test_device(uint8_t Channel, uint8_t ADDRESS);
+    String scan(void);
+  private:
+    uint8_t _address, _channel;
+    uint8_t _devAddress[128];
+    
+    bool test_I2C(uint8_t ADDRESS);
+    bool ifInList(uint8_t ADDRESS);
+    uint8_t write_I2C(uint8_t value);
+};
+#endif
